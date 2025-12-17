7a0ef1b782b53c453f5bb14794714265cb8e1ae2
Farrux Temurov <tfarrux70@gmail.com>
2025-12-17 11:57:41 +0100

    add a replaceWord method

---
+++ Basics/LoopsAndString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsAndString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReplaceMethod rm = new ReplaceMethod();
+
+            string text = "Hello world";
+            string output = rm.ReplaceWord(text, "world", "Everyone");
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Basics/LoopsAndString/ReplaceMethod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsAndString
+{
+    public class ReplaceMethod
+    {
+        public string ReplaceWord(string input, string search, string replacement)
+        {
+            string result = "";
+            int i = 0;
+
+            while (i <= input.Length - search.Length)
+            {
+                if (input.Substring(i, search.Length) == search)
+                {
+                    result += replacement;
+                    i += search.Length;
+                }
+                else
+                {
+                    result += input[i];
+                    i++;
+                }
+            }
+
+            while (i < input.Length)
+            {
+                result += input[i];
+                i++;
+            }
+            return result;
+        }
+    }
+}

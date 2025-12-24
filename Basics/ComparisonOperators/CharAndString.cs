d1f307d05fe096ff32e41e452acffe7690e5713d
Farrux Temurov <tfarrux70@gmail.com>
2025-12-24 10:52:10 +0100

    restrict the user from writing more than one letter

--- Basics/ComparisonOperators/CharAndString.cs
+++ Basics/ComparisonOperators/CharAndString.cs
@@ -14,12 +14,24 @@ namespace ComparisonOperators
 
             for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine($"Enter the letter number {i + 1}");
-                letters[i] = Console.ReadLine()[0];
+                string input;
+                do
+                {
+                    Console.WriteLine($"Enter the letter number {i + 1}");
+                    input = Console.ReadLine();
+
+                    if (input.Length != 1)
+                    {
+                        Console.WriteLine("Please enter only ONE letter!");
+                    }
+
+                } while (input.Length != 1);
+
+                letters[i] = input[0];
+
             }
 
             Array.Reverse(letters);
-
             string reversedLetters = new string(letters); // convert char to string
             Console.WriteLine($"Reversed order of letters are {reversedLetters}");
         }

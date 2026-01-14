022c5020ca11fe20b22b66a0cffd37d6d7ae7869
Farrux Temurov <tfarrux70@gmail.com>
2026-01-14 18:08:58 +0100

    implement add method with dynamic array

---
+++ Basics/Methods/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static int Add()
+        {
+            List<int> arr = new List<int>();
+            string input;
+
+            Console.WriteLine("Enter numbers (press X to calculate)");
+
+            while (true)
+            {
+                Console.Write("Input number: ");
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "x") break;
+
+                arr.Add(int.Parse(input));
+            }
+
+            int sum = 0;
+            foreach (int n in arr)
+            {
+                sum += n;
+            }
+
+            return sum;
+        }
+
+        static
+
+        static void Menu()
+        {
+            int choice;
+            String con;
+            do
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1: Add numbers," +
+                    " 2: Find a Combination of numbers");
+                Console.Write("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        int result = Add();
+                        Console.WriteLine($"sum = {result}");
+                        break;
+                    default: Console.WriteLine("Wrong choice!"); break;
+                }
+                Console.WriteLine("Do you want to continue?(y/n)");
+                con = Console.ReadLine();
+            } while (con.Equals("y"));
+            Console.WriteLine("Thanks for your visit!");
+        }
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+    }
+}

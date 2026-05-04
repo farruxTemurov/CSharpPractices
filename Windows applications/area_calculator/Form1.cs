d4f9f5703e0506c05c1f4c28c2ee7da2eefc1862
Farrux Temurov <tfarrux70@gmail.com>
2026-05-04 12:05:40 +0200

    add a simple form to calculate area of a rectangle using encapsulation.

---
+++ Windows applications/area_calculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace area_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Rectangle r = new Rectangle();
+            r.A = Convert.ToInt32(textBox1.Text);
+            r.B = Convert.ToInt32(textBox2.Text);
+            textBox3.Text = r.Area().ToString();
+        }
+    }
+}
---
+++ Windows applications/area_calculator/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area_calculator
+{
+    internal class Rectangle
+    {
+        private int a;
+        public int A
+        {
+            set { a = value >= 0 ? value : -value; }
+            get { return a; }
+        }
+
+        private int b;
+        public int B
+        {
+            set { b = value >= 0 ? value : -value; }
+            get { return b; }
+        }
+
+        public int Area()
+        {
+            return a * b;
+        }
+    }
+}
